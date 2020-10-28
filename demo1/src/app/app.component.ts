a004aa62e4baefc796b5a0620261274e77faf229
Norman Torres <norman.torres.mx@gmail.com>
2020-10-28 16:44:22 -0600

    ngIf, ngFor, ngSwitch

---
+++ demo1/src/app/app.component.ts
@@ -0,0 +1,52 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+    // items: any = {
+  //   imagenUrl: 'https://www.purina-latam.com/sites/g/files/auxxlc391/files/styles/facebook_share/public/Que_debes_saber_antes_de_adoptar_un_gatito.jpg?itok=btO4gas0'
+  // }
+  // name = 'Normbre';
+  name = "Norman"
+  // array = ['🍏', '🍎', '🍌', '🍑']
+  // input = ''
+
+  // addItem() {
+  //   this.array.push(this.input)
+  //   this.input = ''
+  // }
+  // deleteItem(index: number) {
+  //   this.array.splice(index, 1)
+  // }
+
+  // products = [
+  //   {
+  //     id: 1,
+  //     image: 'hola',
+  //     title: 'Camisa',
+  //     price: 80,
+  //     description: 'bla bla bla bla'
+  //   },
+  //   {
+  //     id: 2,
+  //     image: '',
+  //     title: 'Hoodie',
+  //     price: 120,
+  //     description: 'bla bla bla bla'
+  //   },
+  //   {
+  //     id: 3,
+  //     image: '',
+  //     title: 'Taza',
+  //     price: 90,
+  //     description: 'bla bla bla bla'
+  //   }
+  // ]
+  // miFuncion (price) {
+  //   alert(price)
+  // }
+
+}
